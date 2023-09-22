4c63a545000702f2aba2831e5d81b38d63846c68
フカシ はたけ <89567210+fukashi-hatake@users.noreply.github.com>
2023-09-22 16:30:49 +0500

    Update data.py

--- data.py
+++ data.py
@@ -3,8 +3,9 @@ import pandas as pd
 
 def load_data(): 
     # Specify the URL of the publicly accessible Google Sheet
-    sheet_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRLTgxkwclFqHFN6qSfGfYhNgUyK4lyISq5nMyp43tLBl2ICiwbTnvPdfJyKypHe4K8fUhKLrzA7sSd/pub?gid=0&single=true&output=csv'
+    sheet1_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRLTgxkwclFqHFN6qSfGfYhNgUyK4lyISq5nMyp43tLBl2ICiwbTnvPdfJyKypHe4K8fUhKLrzA7sSd/pub?gid=0&single=true&output=csv'
+    sheet2_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRLTgxkwclFqHFN6qSfGfYhNgUyK4lyISq5nMyp43tLBl2ICiwbTnvPdfJyKypHe4K8fUhKLrzA7sSd/pub?gid=1641625318&single=true&output=csv'
+    newDF = pd.read_csv(sheet1_url)
+    oldDF = pd.read_csv(sheet2_url)
 
-    df = pd.read_csv(sheet_url)
-
-    return df 
+    return newDF, oldDF
