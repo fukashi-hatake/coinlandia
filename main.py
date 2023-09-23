85908e63fc26ff8cea5e06fb660a9b9d64980a29
フカシ はたけ <89567210+fukashi-hatake@users.noreply.github.com>
2023-09-23 21:34:36 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,19 @@ import data
 def main(): 
     st.markdown('<h1 style="color:#4B7CA7;font-size:24px;">Coin Collection of Firuz</h1>', unsafe_allow_html=True) 
     st.write("")
+
+    st.sidebar.info("Montherland: **Uzbekistan** 🇺🇿")
     
     coin_data_new, coin_data_old, short_list_data = data.load_data()
 
     total_number = coin_data_new['Count'].sum() + coin_data_old['Count'].sum() + short_list_data['Count'].sum()
     total_types  = coin_data_new['Type'].sum()  + coin_data_old['Type'].sum() + short_list_data['Type'].sum()
 
-    st.info("Total number of coins: **{}**".format(total_number), icon='💰')
+    number_of_countries = coin_data_new.shape[0] + short_list_data.shape[0]
+
+    st.sidebar.info("Number of countries: {}".format(number_of_countries), icon='🌏')
+
+    st.info("Total number of coins: **{}**".format(total_number), icon='🪙')
     st.info("Total number of coin types: **{}**".format(total_types))
 
     st.write("List of top countries and number of coins")
