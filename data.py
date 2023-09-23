478b83f7d41708a48d9c7679da2e876eb5c65fd2
フカシ はたけ <89567210+fukashi-hatake@users.noreply.github.com>
2023-09-23 23:06:22 +0500

    Update data.py

--- data.py
+++ data.py
@@ -6,8 +6,10 @@ def load_data():
     sheet1_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRLTgxkwclFqHFN6qSfGfYhNgUyK4lyISq5nMyp43tLBl2ICiwbTnvPdfJyKypHe4K8fUhKLrzA7sSd/pub?gid=0&single=true&output=csv'
     sheet2_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRLTgxkwclFqHFN6qSfGfYhNgUyK4lyISq5nMyp43tLBl2ICiwbTnvPdfJyKypHe4K8fUhKLrzA7sSd/pub?gid=1641625318&single=true&output=csv'
     sheet3_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRLTgxkwclFqHFN6qSfGfYhNgUyK4lyISq5nMyp43tLBl2ICiwbTnvPdfJyKypHe4K8fUhKLrzA7sSd/pub?gid=1983727577&single=true&output=csv'
+    sheet4_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRLTgxkwclFqHFN6qSfGfYhNgUyK4lyISq5nMyp43tLBl2ICiwbTnvPdfJyKypHe4K8fUhKLrzA7sSd/pub?gid=1973994839&single=true&output=csv'
     newDF = pd.read_csv(sheet1_url)
     oldDF = pd.read_csv(sheet2_url)
     short_listDF = pd.read_csv(sheet3_url)
+    one_listDF = pd.read_csv(sheet4_url)
 
-    return newDF, oldDF, short_listDF
+    return newDF, oldDF, short_listDF, one_listDF
