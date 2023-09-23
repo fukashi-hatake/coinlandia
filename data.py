a19aec8ec85b7b10f08e6a11bd1b74cea543d8a9
フカシ はたけ <89567210+fukashi-hatake@users.noreply.github.com>
2023-09-23 21:20:36 +0500

    Update data.py

--- data.py
+++ data.py
@@ -5,7 +5,7 @@ def load_data():
     # Specify the URL of the publicly accessible Google Sheet
     sheet1_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRLTgxkwclFqHFN6qSfGfYhNgUyK4lyISq5nMyp43tLBl2ICiwbTnvPdfJyKypHe4K8fUhKLrzA7sSd/pub?gid=0&single=true&output=csv'
     sheet2_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRLTgxkwclFqHFN6qSfGfYhNgUyK4lyISq5nMyp43tLBl2ICiwbTnvPdfJyKypHe4K8fUhKLrzA7sSd/pub?gid=1641625318&single=true&output=csv'
-    sheet3_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRLTgxkwclFqHFN6qSfGfYhNgUyK4lyISq5nMyp43tLBl2ICiwbTnvPdfJyKypHe4K8fUhKLrzA7sSd/pubhtml?gid=1983727577&single=true'
+    sheet3_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRLTgxkwclFqHFN6qSfGfYhNgUyK4lyISq5nMyp43tLBl2ICiwbTnvPdfJyKypHe4K8fUhKLrzA7sSd/pub?gid=1983727577&single=true&output=csv'
     newDF = pd.read_csv(sheet1_url)
     oldDF = pd.read_csv(sheet2_url)
     short_listDF = pd.read_csv(sheet3_url)
